2f73d67e528b182f1eda267d2e2e3ba8f322feee
Sascha Dittmann <Sascha5812@gmx.de>
2021-07-15 18:09:36 +0200

    fix: add specific azure-mgmt-resource package

--- setup/install_requirements.sh
+++ setup/install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python --version
 pip install --upgrade azure-cli
-pip install --upgrade azureml-sdk
+pip install --upgrade azureml-sdk azure-mgmt-resource==18.0.0
 pip install -r requirements.txt
